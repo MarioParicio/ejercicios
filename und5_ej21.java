ae32635ed41ce237077aa771ce19b08d413da4d5
Amo a Venezuela <68949370+MarioParicio@users.noreply.github.com>
2021-11-29 16:54:29 +0100

    Update und5_ej21.java

--- und5_ej21.java
+++ und5_ej21.java
@@ -1,12 +1,14 @@
 package und5;
 import java.util.Scanner;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 public class und5_ej21 {
+
         static Scanner sc = new Scanner(System.in);
+        static String sentence = "";
+        
         public static void main(String[] args) {
+
             System.out.println("Escribe una frase");
-            String sentence = sc.nextLine().trim();
+            sentence = sc.nextLine().trim();
 
             System.out.println(sentence.toUpperCase());
 
@@ -15,7 +17,7 @@ public class und5_ej21 {
             System.out.print(Character.toUpperCase(sentence.charAt(0)));
             for ( int i = 1; i < sentence.length(); i++){
 
-                System.out.print(sentence.charAt(i));
+                System.out.print(Character.toLowerCase(sentence.charAt(i)));
                 if (sentence.charAt(i) ==  ' ') {
                     int x = i + 1;
                     System.out.print(Character.toUpperCase(sentence.charAt(x)));
