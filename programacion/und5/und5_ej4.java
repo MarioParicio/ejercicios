696354d6da0e62c559fd36ac340604773643ca8d
Amo a Venezuela <68949370+MarioParicio@users.noreply.github.com>
2021-12-12 19:44:47 +0100

    Delete programacion directory

--- programacion/und5/und5_ej4.java
+++
@@ -1,9 +0,0 @@
-package und5;
-
-public class und5_ej4 {
-    static String var[];
-    public static void main(String[] args) {
-        String[] var= {"variables", "literales", "constantes", "expresiones", "métodos", "bloques", "excepciones"};
-        System.out.println(var[1]);
-    }
-}
